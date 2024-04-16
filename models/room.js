1db1b63e4c5d08e1fe5e7cf5c9c82f01709ca354
achal sayee <achalsayee2@gmail.com>
2024-04-16 21:53:49 +0530

    active days added

---
+++ models/room.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://0.0.0.0:27017/mastiplay1', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}); 
+
+const roomSchema = new mongoose.Schema({
+    id: String,
+    admin: String,
+    askBeforeJoining: { type: Boolean, default: false },
+    roomType: Number,
+    name: String,
+    imgUrl: { type: String, default: null },
+    announcement: { type: String, default: null }
+}, {
+    timestamps: true,
+});
+
+const Room = mongoose.model('Room', roomSchema);
+
+module.exports = { Room, mongoose };
