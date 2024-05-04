a152ca62709e3598f549590e92d2c9272f04cbe0
achal sayee <achalsayee2@gmail.com>
2024-05-04 22:32:36 +0530

    admin change

--- models/room.js
+++ models/room.js
@@ -1,22 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://0.0.0.0:27017/mastiplay', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}); 
+mongoose.connect("mongodb://0.0.0.0:27017/mastiplay", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-const roomSchema = new mongoose.Schema({
+const roomSchema = new mongoose.Schema(
+  {
     id: String,
-    admin: [String],
+    // admin: [String],
+    admin: String,
+
     askBeforeJoining: { type: Boolean, default: false },
     roomType: Number,
     name: String,
     imgUrl: { type: String, default: null },
-    announcement: { type: String, default: null } 
-}, {
+    announcement: { type: String, default: null },
+  },
+  {
     timestamps: true,
-});
+  }
+);
 
-const Room = mongoose.model('Room', roomSchema);
+const Room = mongoose.model("Room", roomSchema);
 
 module.exports = { Room, mongoose };
